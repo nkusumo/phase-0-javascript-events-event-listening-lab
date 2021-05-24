eaef4cb03635fdbaba66ded833c7ec736432fa18
nkusumo <nis.kusumo@gmail.com>
2021-05-24 10:13:40 -0700

    commit tree 889667533fdd9cb4861e5bc914347a903b5f5957

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 function addingEventListener() {
     const input = document.getElementById('input');
-    input.addEventListener('click', function() {
-        alert('I was clicked!');
-    });
+    input.addEventListener('click', () => alert('I was clicked!'));
 }
 
 addingEventListener();
